abb4f83cad879683afa2f1d77b7249071185b6f8
ANKUR KUMAR <=>
2025-09-03 08:22:42 +0530

    add the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,43 @@
+import Image from "next/image"
+import Link from "next/link"
 
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mt-24 bg-gray-200 text-sm">
+      {/* TOP */}
+      <div className="flex justify-between gap-24">
+        {/* LEFT */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+        <Link href="/">
+        <div className="text-2xl tracking-wide">LAMA</div>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, dolore.
+        </p>
+        <span className="font-semibold">ankurkumar6633@gmail.com</span>
+        <span className="font-semibold">+91 707 075 2364</span>
+        <div className="flex gap-6">
+          <Image src="/facebook.png" alt="" width={16} height={16} />
+          <Image src="/facebook.png" alt="" width={16} height={16} />
+          <Image src="/facebook.png" alt="" width={16} height={16} />
+          <Image src="/facebook.png" alt="" width={16} height={16} />
+          <Image src="/facebook.png" alt="" width={16} height={16} />
+        </div>
+        </div>
+        {/* CENTER */}
+        <div className="w-1/2"></div>
+        {/* RIGHT */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p>Be the first to get the latest news about trends, promotions, and much more!</p>
+          <div className="flex">
+            <input type="text" placeholder="Email Address" className="p-4 w-3/4" />
+          </div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className=""></div>
     </div>
   )
 }
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -34,26 +34,27 @@ const slides = [
 const Slider = () => {
   const [current, setCurrent] = useState(0);
 
-//   useEffect (() => {
-//     const interval = setInterval(() => {
-//         setCurrent((prev) => (prev === slides.length -1 ? 0 : prev + 1));
-//     },5000);
+  //   useEffect (() => {
+  //     const interval = setInterval(() => {
+  //         setCurrent((prev) => (prev === slides.length -1 ? 0 : prev + 1));
+  //     },5000);
 
-//     return () => clearInterval(interval);
-//   }, []);
+  //     return () => clearInterval(interval);
+  //   }, []);
 
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000"
-      style={{transform: `translateX(-${current * 100}vw)`}}
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
       >
         {slides.map((slide) => (
           <div
-            className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            className={`${slide.bg} w-screen h-full flex flex-col gap-16 lg:flex-row`}
             key={slide.id}
           >
             {/* TEXT CONTENT */}
-            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+            <div className="h-1/2 lg:w-1/2 lg:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
               <h2 className="text-xl lg:text-3xl 2xl:text-5xl font-semibold">
                 {slide.description}
               </h2>
@@ -67,7 +68,7 @@ const Slider = () => {
               </Link>
             </div>
             {/* IMAGE CONTENT */}
-            <div className="h-1/2 xl:w-1/2 xl:h-full relative">
+            <div className="h-1/2 lg:w-1/2 lg:h-full relative">
               <Image
                 src={slide.img}
                 alt=""
@@ -80,17 +81,19 @@ const Slider = () => {
         ))}
       </div>
       <div className="absolute m-auto left-1/2 bottom-8 gap-4 flex">
-      {slides.map((slide, index) => (
+        {slides.map((slide, index) => (
           <div
-          className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
-            current === index ? "scale-150" : ""
+            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
             }`}
             key={slide.id}
             onClick={() => setCurrent(index)}
-            >
-            { current === index && (<div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>)}
-        </div>
-      ))}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
